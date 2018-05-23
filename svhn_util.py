2ead82c03ed995dc4e9380b7a553d437bfbba01c
LukeZhuang <cg260sbh@cg260sbh-17392.pods.cg260sbh.svc.cluster.local>
2018-05-23 16:44:43 -0700

    finish building SVHN and Celeba model

---
+++ svhn_util.py
@@ -0,0 +1,119 @@
+"""Functions for downloading and reading SVHN data."""
+"""
+Modefied from Tensorflow MNIST dataset: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/learn/python/learn/datasets/mnist.py
+"""
+
+import numpy
+import os
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import random_seed
+from tensorflow.python.platform import gfile
+
+
+def extract_images(f):
+  data=numpy.load(f)
+  return data
+
+
+class DataSet(object):
+
+  def __init__(self,
+               images,
+               dtype=dtypes.float32,
+               reshape=True,
+               seed=None):
+    """Construct a DataSet.
+    one_hot arg is used only if fake_data is true.  `dtype` can be either
+    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+    `[0, 1]`.  Seed arg provides for convenient deterministic testing.
+    """
+    seed1, seed2 = random_seed.get_seed(seed)
+    # If op level seed is not set, use whatever graph level seed is returned
+    numpy.random.seed(seed1 if seed is None else seed2)
+    dtype = dtypes.as_dtype(dtype).base_dtype
+    if dtype not in (dtypes.uint8, dtypes.float32):
+      raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                      dtype)
+
+    self._num_examples = images.shape[0]
+
+    # Convert shape from [num examples, rows, columns, depth]
+    # to [num examples, rows*columns] (assuming depth == 1)
+    if reshape:
+      images = images.reshape(images.shape[0],
+                                images.shape[1] * images.shape[2] * images.shape[3])
+    if dtype == dtypes.float32:
+      # Convert from [0, 255] -> [0.0, 1.0].
+      images = images.astype(numpy.float32)
+      images = numpy.multiply(images, 1.0 / 255.0)
+    self._images = images
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size, fake_data=False, shuffle=True):
+    """Return the next `batch_size` examples from this data set."""
+    start = self._index_in_epoch
+    # Shuffle for the first epoch
+    if self._epochs_completed == 0 and start == 0 and shuffle:
+      perm0 = numpy.arange(self._num_examples)
+      numpy.random.shuffle(perm0)
+      self._images = self.images[perm0]
+    # Go to the next epoch
+    if start + batch_size > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Get the rest examples in this epoch
+      rest_num_examples = self._num_examples - start
+      images_rest_part = self._images[start:self._num_examples]
+      # Shuffle the data
+      if shuffle:
+        perm = numpy.arange(self._num_examples)
+        numpy.random.shuffle(perm)
+        self._images = self.images[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size - rest_num_examples
+      end = self._index_in_epoch
+      images_new_part = self._images[start:end]
+      return numpy.concatenate((images_rest_part, images_new_part), axis=0)
+    else:
+      self._index_in_epoch += batch_size
+      end = self._index_in_epoch
+      return self._images[start:end]
+
+
+def read_data_sets(train_dir,
+                   dtype=dtypes.float32,
+                   reshape=True,
+                   seed=None):
+
+  TRAIN_IMAGES = 'svhn_all.npy'
+
+  filepath = os.path.join(train_dir, TRAIN_IMAGES)
+  with gfile.Open(filepath, 'rb') as f:
+    train_images = extract_images(f)
+
+
+  options = dict(dtype=dtype, reshape=reshape, seed=seed)
+
+  train = DataSet(train_images, **options)
+
+  return train
+
+
+def load_svhn(train_dir='svhn'):
+  return read_data_sets(train_dir)
